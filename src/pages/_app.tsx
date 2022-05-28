8b9a96976d9d045f44df15da9eca9ac1843f0cac
ivankrzak <krzak.ivan@gmail.com>
2022-05-28 18:07:22 +0200

    Create apollo client and FE codegen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
+import { ApolloProvider } from '@apollo/client'
 import { ChakraProvider } from '@chakra-ui/react'
+import apolloClient from 'lib/apollo'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider>
-    <Component {...pageProps} />
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
   </ChakraProvider>
 )
 
